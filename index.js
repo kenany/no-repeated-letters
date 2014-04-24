cca927bbe207665baad63f153d1984f9c3794b42
Kenan Yildirim <kenan@kenany.me>
2014-04-24 11:41:56 -0400

    Minor optimization

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var contains = require('lodash.contains');
 
 function noRepeatedLetters(word) {
-  for (var i = 0; i < word.length; i++) {
+  for (var i = 0, length = word.length; i < length; i++) {
     if (contains(word.slice(0, i), word.charAt(i))) {
       return false;
     }
