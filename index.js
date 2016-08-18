57e2ca525eb657a62df73e7ba085e426dec5c85e
Kenan Yildirim <kenan@kenany.me>
2016-08-18 15:08:03 -0400

    deps: lodash.includes@^4.3.0
    Closes https://github.com/KenanY/no-repeated-letters/issues/4

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var contains = require('lodash.contains');
+var includes = require('lodash.includes');
 
 function noRepeatedLetters(word) {
   for (var i = 0, length = word.length; i < length; i++) {
-    if (contains(word.slice(0, i), word.charAt(i))) {
+    if (includes(word.slice(0, i), word.charAt(i))) {
       return false;
     }
   }
