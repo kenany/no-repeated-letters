cd8c5987213fbf07975d4cecc00fbcf427154c51
Kenan Yildirim <kenan@kenany.me>
2019-08-18 11:48:32 -0400

    deps: eslint@5.16.0, eslint-config@6.0.0
    Closes https://github.com/KenanY/no-repeated-letters/pull/41

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
 import buble from 'rollup-plugin-buble';
 
 const pkg = require('./package.json');
+
 const external = Object.keys(pkg.dependencies);
 
 export default {
   entry: 'index.js',
   plugins: [
     buble({
-      target: {node: 4}
+      target: { node: 4 }
     })
   ],
   external: external,
