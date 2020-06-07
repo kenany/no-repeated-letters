c0f4a795f22ddd9ee938419eadca8784657b6d97
Kenan Yildirim <kenan@kenany.me>
2020-06-07 15:30:10 -0400

    feat(deps): rollup@2.14.0, @rollup/plugin-buble@0.21.3
    BREAKING CHANGE: Node.js v10 is now the minimum supported version of
    Node.js.
    
    Closes https://github.com/KenanY/no-repeated-letters/pull/57
    Closes https://github.com/KenanY/no-repeated-letters/pull/62

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,24 @@
-import buble from 'rollup-plugin-buble';
+import buble from '@rollup/plugin-buble';
 
 const pkg = require('./package.json');
 
 const external = Object.keys(pkg.dependencies);
 
 export default {
-  entry: 'index.js',
+  input: 'index.js',
   plugins: [
     buble({
-      target: { node: 4 }
+      target: { node: '8.10' }
     })
   ],
   external: external,
-  targets: [
+  output: [
     {
-      dest: pkg.main,
+      file: pkg.main,
       format: 'cjs'
     },
     {
-      dest: pkg['jsnext:main'],
+      file: pkg['jsnext:main'],
       format: 'es'
     }
   ]
